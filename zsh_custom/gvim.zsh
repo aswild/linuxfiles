554d8dcfe09e08410864b67579a130e16d9b6497
Allen <wildman279@gmail.com>
2015-09-26 00:33:19 -0400

    fix gvim() compatibility to auto-detect Windows home directory

--- zsh_custom/gvim.zsh
+++ zsh_custom/gvim.zsh
@@ -1,10 +1,11 @@
 #! /bin/zsh
 
 if [[ -e /cygdrive/c/Program\ Files\ \(x86\)/Vim/vim74/gvim.exe ]]; then
+    export WINDOWS_HOME='C:\Users\'${HOME##*/}
     function gvim()
     {
         for i in "$@"; do
-            HOME='C:\Users\Allen' cygstart '/cygdrive/c/Program Files (x86)/Vim/vim74/gvim.exe' --remote-tab-silent \"$i\"
+            HOME=$WINDOWS_HOME cygstart '/cygdrive/c/Program Files (x86)/Vim/vim74/gvim.exe' --remote-tab-silent \"$i\"
         done
     }
 fi
