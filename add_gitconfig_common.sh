f5ea84014ea52aa3c7247c7262d1f23d10bc735c
Allen <aswild@gatech.edu>
2015-11-19 22:00:15 -0500

    make executable, and cut out for figuring out allowing deletion

--- add_gitconfig_common.sh
+++ add_gitconfig_common.sh
@@ -3,10 +3,10 @@
 OPWD=$PWD
 cd ~
 
-echo "symlinking .gitconfig_common"
-ln -s linuxfiles/.gitconfig_common ~/.gitconfig_common
-
-echo -e "\n[include]\n    file = ~/.gitconfig_common" >> ~/.gitconfig
+#echo "symlinking .gitconfig_common"
+#ln -s linuxfiles/.gitconfig_common ~/.gitconfig_common
+#
+#echo -e "\n[include]\n    file = ~/.gitconfig_common" >> ~/.gitconfig
 
 echo "Press enter to delete this script, or ^C to abort"
 read foo
