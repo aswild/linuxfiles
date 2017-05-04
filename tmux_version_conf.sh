e1e09166269ad6604b3774aad1b938fdf6bea098
Allen Wild <allenwild93@gmail.com>
2017-05-04 13:31:37 -0400

    tmux: update configs for v2.4 compatibility

--- tmux_version_conf.sh
+++ tmux_version_conf.sh
@@ -1,7 +1,32 @@
 #! /bin/bash
 
+# returns whether $version >= $1
+# unlike bash (( arith )) comparisons, expr supports floats
+ver_at_least() {
+    return $(echo "$version $1" | awk '{if ($1 >= $2) print 0; else print 1}')
+}
+
 version=$(tmux -V | sed 's/tmux\s*//')
-if [[ $(echo "$version" | awk '{if ($1 >= 2.1) print 1; else print 0;}') == "1" ]]; then
+
+if ver_at_least 2.4 ; then
+    # 2.4 copy mode bindings
+    tmux bind -T copy-mode-vi v                 send -X begin-selection
+    tmux bind -T copy-mode-vi y                 send -X copy-selection-and-cancel
+    tmux bind -T copy-mode-vi Space             send -X clear-selection
+    tmux bind -T copy-mode-vi Escape            send -X cancel
+    tmux bind -T copy-mode-vi MouseDragEnd1Pane send -X copy-selection
+else
+    # pre-2.4 copy mode bindings
+    tmux bind -t vi-copy v                  begin-selection
+    tmux bind -t vi-copy y                  copy-selection
+    tmux bind -t vi-copy Space              clear-selection
+    tmux bind -t vi-copy Escape             cancel
+    tmux bind -t vi-edit Escape             cancel
+    tmux bind -ct vi-edit Escape            cancel
+    tmux bind -t vi-copy MouseDragEnd1Pane  copy-selection -x
+fi
+
+if ver_at_least 2.1 ; then
     # tmux >= 2.1 settings
     tmux set-option -g mouse on
     tmux bind -n WheelUpPane if-shell -F -t = "#{mouse_any_flag}" "send-keys -M" \
