bcfca7b77e40bdaea7ab940b8aac2ef8960c43d8
Allen Wild <allenwild93@gmail.com>
2023-08-08 11:21:56 -0400

    zwild: refactor git_prompt_info
    The big short-circuit chain of command substitution was kinda hard to
    reason about, the sed magic to set exit codes was flaky, and there were
    cases where if no refs were present then the prompt wouldn't even show
    the abbreviated commit hash.
    
    Fix these problems by spreading out the code into a series of if
    statements, and use oh-my-zsh's __git_prompt_git wrapper which disables
    optional locks.

--- zsh_custom/zwild.zsh
+++ zsh_custom/zwild.zsh
@@ -17,20 +17,40 @@ function zsh_nogit() {
 }
 
 function git_prompt_info() {
-    ( [[ "$ZSH_GIT_PROMPT" != "0" && "$PWD" != "$HOME" ]] && git rev-parse --git-dir &>/dev/null ) || return 0
-    if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
-        ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
-            ref=tags/$(command git describe --tags --exact-match HEAD 2>/dev/null) || \
-            ref=$(command git show-ref 2>/dev/null | sed -n '/^'$(command git rev-parse HEAD 2>/dev/null)' refs\/remotes\/\(m\/.*\)/,${s//\1/p;q0};$q1') || \
-            ref=$(command git rev-parse --short HEAD 2> /dev/null)
-        [[ -n "$ref" ]] || return 0
+    # check whether we should even do git prompt info
+    (
+        set -e
+        [[ "$ZSH_GIT_PROMPT" != "0" && "$PWD" != "$HOME" ]]
+        git rev-parse --git-dir
+        [[ "$(__git_prompt_git config --get oh-my-zsh.hide-status)" != 1 ]]
+    ) &>/dev/null || return 0
 
-        # re-order the git prompt so that dirty/clean can be before the current branch
-        if [[ $ZSH_THEME_GIT_PROMPT_DIRTY_BEFORE_BRANCH == "true" ]]; then
-            echo "$ZSH_THEME_GIT_PROMPT_PREFIX$(parse_git_dirty)${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
-        else
-            echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
-        fi
+    local ref tmp
+    # exact branch name
+    ref="$(__git_prompt_git symbolic-ref HEAD 2>/dev/null)"
+    # exact tag name
+    if [[ -z "$ref" ]]; then
+        ref="$(__git_prompt_git describe --tags --exact-match HEAD 2>/dev/null)"
+        [[ -n "$ref" ]] && ref="tags/$ref"
+    fi
+    # repo manifest remote-tracking branch (refs/remotes/m/foobar)
+    if [[ -z "$ref" ]]; then
+        tmp="$(__git_prompt_git rev-parse HEAD 2>/dev/null)"
+        # tricky sed command, cause we need to quit after the first replacement.
+        # this uses a range match, and the substitute is inside the command block, since 'q' isn't
+        # a usable flag for 's'.
+        ref="$(__git_prompt_git show-ref 2>/dev/null | sed -nE "/^${tmp}"' refs\/remotes\/(m\/.*)/{s//\1/p;q}')"
+    fi
+    # no useful refs, just use abbreviated ref
+    if [[ -z "$ref" ]]; then
+        ref="$(__git_prompt_git rev-parse --short HEAD 2>/dev/null)"
+    fi
+
+    [[ -n "$ref" ]] || return 0
+    if [[ $ZSH_THEME_GIT_PROMPT_DIRTY_BEFORE_BRANCH == "true" ]]; then
+        echo "$ZSH_THEME_GIT_PROMPT_PREFIX$(parse_git_dirty)${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    else
+        echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
     fi
 }
 
