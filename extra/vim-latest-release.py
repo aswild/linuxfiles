06074f78802be48f7fe357705db2af48aeda8b73
Allen Wild <allenwild93@gmail.com>
2023-08-15 11:53:19 -0400

    vim-latest-release: update for WSL2
    Remove github api key. You don't need an API key to get this data (as
    long as your IP doesn't hit the rate-limit).
    
    Replace cygstart with wslview to launch the installer. Note that you
    can't launch the exe if saved to a Linux path like /tmp, so use wslvar
    and wslpath to save to the user's %TEMP% directory when possible.

--- extra/vim-latest-release.py
+++ extra/vim-latest-release.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python3
 
-from subprocess import check_call
+"""
+Use GitHub's API to find and download the latest signed 64-bit windows gvim
+installer from the vim-win32-installer repo.
+
+This script requires python >=3.7, the github module, and optionally the wslvar,
+wslpath, and wslview programs from the "wslu" package.
+"""
+
+import subprocess
+import os
 import sys
 
 from github import Github
 
-GH_API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+def cmd_output(*cmd):
+    res = subprocess.run(cmd, stdout=subprocess.PIPE, check=True, text=True)
+    return res.stdout.strip()
+
+def get_tmpdir():
+    if os.path.exists('/mnt/wsl'):
+        try:
+            temp_win = cmd_output('wslvar', 'TEMP')
+            temp_linux = cmd_output('wslpath', temp_win)
+            tmpdir = temp_linux
+            assert os.path.isdir(tmpdir)
+        except:
+            print('Warning: failed to get WSL host TEMP directory')
+            tmpdir = '/tmp'
+    else:
+        tmpdir = '/tmp'
+    assert os.path.isdir(tmpdir), f"tmpdir '{tmpdir}' doesn't exist"
+    return tmpdir
 
 def main():
-    gh = Github(GH_API_KEY)
+    gh = Github()
     repo = gh.get_repo('vim/vim-win32-installer')
     installer = None
     for rel in repo.get_releases():
@@ -21,17 +47,13 @@ def main():
     if installer is None:
         raise Exception('No x86_signed.exe releases found')
 
-    exe_path = f'/tmp/{installer.name}'
+    exe_path = os.path.join(get_tmpdir(), installer.name)
     print(f'Downloading {installer.browser_download_url} to {exe_path}')
-    check_call(['wget', '-O', exe_path, installer.browser_download_url])
-    try:
-        check_call(['chmod', '+x', exe_path])
-    except Exception as e:
-        print(f'WARNING: failed to set {exe_path} as executable')
+    subprocess.run(['wget', '-O', exe_path, installer.browser_download_url], check=True)
 
     ans = input(f'\nDownloaded {installer.name} - launch now? [y/n] ')
     if ans.lower() == 'y':
-        check_call(['cygstart', exe_path])
+        subprocess.run(['wslview', exe_path])
 
 
 if __name__ == '__main__':
